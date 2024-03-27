3be15dba05f3d0f169c87c3bb986f50ea9ed3af6
Niraj436 <nirajkhadka@gmail.com>
2024-03-27 22:50:37 +0545

    How to play added

--- Frontend/src/pages/HowToPlay/HowToPlay.jsx
+++ Frontend/src/pages/HowToPlay/HowToPlay.jsx
@@ -1,12 +1,55 @@
-import React from 'react';
-import Wrapper from './HowToPlay.style';
+import React from "react";
+import Wrapper from "./HowToPlay.style";
 
 const HowToPlay = () => {
+  
   return (
     <Wrapper>
-        <h1>How to play</h1>
-    </Wrapper>
-  )
-}
+      <h3>How to play</h3>
 
-export default HowToPlay
+      <div className="container">
+        <div className="video">
+          <h5 className="video-name">1. Register and login</h5>
+          <iframe
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/XCC8DcYJOIM?autoplay=1?autoplay=false`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="video">
+          <h5 className="video-name">2. How to play vs computer</h5>
+          <iframe
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/5luSsV1TLSM?autoplay=1?autoplay=false`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="video">
+          <h5 className="video-name">3. How to play vs friends</h5>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/kyvr_f3shGI?autoplay=false"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default HowToPlay;
--- Frontend/src/pages/HowToPlay/HowToPlay.style.js
+++ Frontend/src/pages/HowToPlay/HowToPlay.style.js
@@ -1,5 +1,21 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.section`
+    .video{
+        margin: 2rem 0;
+    }
+    .video-name{
+        padding:1rem 0;
+    }
+    .container{
+        display:flex;
+        flex-direction:column;
+        justify-content:center;
+        margin: 0 3rem;
+    }
+    h3{
+        
+
+    }
 `;
 export default Wrapper;
